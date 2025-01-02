83fa0bd1be39746d38281551fa5d0e9142ce60a5
Brendan <brendanjlee5@gmail.com>
2025-01-02 10:57:26 -0800

    init

---
+++ src/test/java/org/example/spring_quiz_application/SpringQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_quiz_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
