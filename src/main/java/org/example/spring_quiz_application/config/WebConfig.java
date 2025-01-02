1597c816835f675a12bc28e96c4d301c15783699
Brendan <brendanjlee5@gmail.com>
2025-01-02 11:03:40 -0800

    Create default config and properties

---
+++ src/main/java/org/example/spring_quiz_application/config/WebConfig.java
@@ -0,0 +1,25 @@
+package org.example.spring_quiz_application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class WebConfig {
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/quiz_application";
+    static final String USER = "root";
+    static final String PASSWORD = "";
+
+    @Bean
+    public DataSource quizDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(JDBC_DRIVER);
+        dataSource.setUrl(DB_URL);
+        dataSource.setUsername(USER);
+        dataSource.setPassword(PASSWORD);
+        return dataSource;
+    }
+}
